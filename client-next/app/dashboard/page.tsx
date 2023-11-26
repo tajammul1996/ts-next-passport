657b61c9bc2af526f2a9b363cdf68af85520735b
tajammul1996 <tpzuffari@gmail.com>
2023-11-26 23:35:09 +0530

    update profile on dashboard

--- client-next/app/dashboard/page.tsx
+++ client-next/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 function DashboardPage() {
   console.log(process.env.NEXT_PUBLIC_SERVER_URL)
+  const [response, setResponse] = useState(null);
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/auth/user`, {
@@ -13,13 +14,14 @@ function DashboardPage() {
     })
       .then((res) => {
         console.log(res)
+        
         return res.json()})
-      .then((data) => console.log(data));
+      .then((data) => setResponse(data));
   });
 
   return (
     <div>
-      {/* <h1>{JSON.stringify(response)}</h1> */}
+      <h1>{JSON.stringify(response)}</h1>
     </div>
   );
 }
