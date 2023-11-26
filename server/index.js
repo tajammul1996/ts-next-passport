cd180996436f1ef9612bbef31bfb20c0e70db7cb
tajammul1996 <tpzuffari@gmail.com>
2023-11-26 19:16:17 +0530

    update server url in callback

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: `/api/auth/github/callback`,
+      callbackURL: `${process.env.SERVER_URL}/api/auth/github/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
       console.log(profile);
